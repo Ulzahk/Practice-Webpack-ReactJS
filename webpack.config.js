1901529c0157a95706d76a415a1bfe744176fa57
Ulzahk <ulzahk@gmail.com>
2021-08-08 12:46:56 -0500

    🎨 [UPDATE] Webpack Dev and Prod enviroments division

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,24 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundel.js",
+    publicPath: "./",
   },
-  mode: "development",
+  mode: "production",
   resolve: {
     extensions: [".js", ".jsx"],
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+    },
   },
   module: {
     rules: [
@@ -41,11 +49,10 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css",
     }),
+    new CleanWebpackPlugin(),
   ],
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    compress: true,
-    open: true,
-    port: 3005,
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
   },
 };
